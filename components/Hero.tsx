e7df91857d1695189724122512e840f83da5576f
Virgil Khayanga <117723011+khayanga@users.noreply.github.com>
2026-01-12 09:41:18 +0300

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -114,18 +114,8 @@ const Hero = () => {
             ))}
           </div>
 
-          {/* Owner */}
-          <p className="animate-fade-up animation-delay-500 text-primary-foreground/80 text-sm pt-6">
-            Founder:{" "}
-            <a
-              href="https://www.facebook.com/share/1CGZHVXGFu/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-semibold underline hover:text-primary-foreground"
-            >
-              Nereah Joan
-            </a>
-          </p>
+        
+         
 
         </div>
       </div>
